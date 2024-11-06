01820a57caeae4cededf413a424baafee65bbb85
Stepan Potiienko <kotnexjieb@protonmail.com>
2024-11-06 08:56:00 +0200

    nvchad config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,27 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    go = { "gofumpt", "goimports-reviser", "golines" },
+    python = { "black", "isort" },
+    c_cpp = { "clang-format" },
+    c = { "clang_format" },
+    cpp = { "clang_format" },
   },
-
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  formatters = {
+      ["goimports-reviser"] = {
+          prepend_args = { "-rm-unused" },
+      },
+      golines = {
+          prepend_args = { "--max-len=80" },
+      },
+    },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,13 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
+  {
+    'adelarsq/image_preview.nvim',
+    event = 'VeryLazy',
+    config = function()
+        require("image_preview").setup()
+    end
+  },
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
@@ -12,14 +18,43 @@ return {
       require "configs.lspconfig"
     end,
   },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "go", "gosum", "gomod", "gowork"
+  		},
+  	  highlight = {
+        enable = true,
+        use_languagetree = true,
+      },
+      indent = {enable=true},
+    },
+  },
 }
